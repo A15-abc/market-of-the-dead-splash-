9533ff57ceb1e90f30fbb97a43e261902b2fcdbf
A <93423567+A15-abc@users.noreply.github.com>
2023-05-20 16:15:32 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+
+function preload(){
+    splashimg=loadImage("splashimage.gif")
+    }
+    
+    function setup(){
+    createCanvas(windowWidth/2,windowHeight)
+    
+    }
+    
+    function draw(){
+    background(splashimg)
+        
+    }
